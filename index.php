f9511e809e7b82c8452b1b757c772b4e79b9629c
Rahmatullah <108264731+delazar1@users.noreply.github.com>
2022-10-21 23:42:00 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,58 +1,71 @@
-<?php
-// $error="";
-$x="";
-$y="";
-$result="";
-// the  isset method  to call the function only when the buttons are clicked
-if (isset($_POST['operation'])){
-    //I am accessing whatever the user inputs by means of the super global $_POST, then stroring in $x and $y
-    $x=$_POST['num1'];
-    $y=$_POST['num2'];
-    $operation=$_POST['operation'];
-
-    switch($operation)
-    {
-        case 'add':
-            $result= $x+$y;
-            break;
-        case 'sub':
-             $result= $x-$y;
-               break;
-         case 'prod':
-            $result= $x*$y;
-                break;
-        case 'divi':
-            $result= $x/$y;
-                break;
-        // default :
-        //    echo  "Invalid Operation";
-    }
-}
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-<title> Simple Calculator</title>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <title>Calculutor</title>
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
 </head>
+
 <body>
-<!-- ?=$_SERVER['PHP_SELF']? -->
-    <form action="calcProcessor.php" method="POST" name="">
-        <h2><?= $error ?></h2>
-       <label for="txt1"> Enter First Number </label> :
-       <input id= "txt1" type="text" name="num1" required><br> <br>
-       <label for="txt2"> Enter Second Number </label>
-       <input id="txt2" type="text" name="num2" required><br> <br>
-       <!-- <label for="rslt"> Result: </label>       
-       <input id= "rslt"  name="result" value= "<?=$result ?>" disabled ><br> <br> -->
-       <input type=submit value= "add" name="operation">
-       <input type=submit value= "sub" name="operation">
-       <input type=submit value= "prod" name="operation">
-       <input type=submit value= "divi" name="operation">
-       
 
+  <h2>Calculator</h2>
+
+  <div class="container">
+    <form action="index.php" method="post">
+      <label for="nbr1">Number 1</label>
+      <input type="text" id="nbr1" name="number1" placeholder="Enter number">
+
+      <label for="nbr2">Number 2</label>
+      <input type="text" id="nbr2" name="number2" placeholder="Enter number">
+
+      <label for="operator">Operator</label>
+      <select id="operator" name="operator" value="Choose an operator ">
+        <option value="addition">Addition</option>
+        <option value="subtraction">Subtraction</option>
+        <option value="multiplication">Multiplication</option>
+        <option value="division">Division</option>
+      </select>
+  
+      <input type="submit" name="validation" value="CALCULATE">
     </form>
-</body>
 
+    <?php
+        if(isset($_POST['validation'])){
+        if ( is_numeric ($_POST['number1']) AND is_numeric ($_POST['number2'])) {
+          switch($_POST['operator'])
+          {
+              case "addition":
+                  $add = $_POST['number1'] + $_POST['number2'];
+                  echo "<hr style='margin-top:8%;'><h3 style='color:rgb(11, 160, 93); font-size: 35px; margin-left:22%;'>Result: ".$add."</h3>";
+                  break;
+              case "subtraction":
+                  $sou = $_POST['number1'] - $_POST['number2'];
+                  echo "<hr style='margin-top:8%;'><h3 style='color:rgb(11, 160, 93); font-size: 35px; margin-left:22%;'>Result: ".$sou."</h3>";
+                  break;
+              case "multiplication":
+                  $mul = $_POST['number1'] * $_POST['number2'];
+                  echo "<hr style='margin-top:8%;'><h3 style='color:rgb(11, 160, 93); font-size: 35px; margin-left:22%;'>Result: ".$mul."</h3>";
+                  break;
+              case "division":
+                  $div = $_POST['number1'] / $_POST['number2'];
+                  echo "<hr style='margin-top:8%;'><h3 style='color:rgb(11, 160, 93); font-size: 35px; margin-left:22%;'>Result: ".$div."</h3>";
+                  break;
+              
+          }
+       }
+       elseif( is_string($_POST['number1']) AND is_string($_POST['number2']) ){
+         echo "<hr style='margin-top:8%;'><h3 style='color:red; font-size: 35px; margin-left:4%;'>Invalid value !</h3>";
+       }
+      }
+    ?>
+
+  </div>
+
+</body>
 </html>
